edcf454684a1b4cf36d8ab1a6ab462f0b38dec3f
saurabhpav <120093700+saurabhpav@users.noreply.github.com>
2022-12-08 16:46:22 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatModule } from './mat/mat.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatModule,
+    BrowserAnimationsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+
+
+
+
